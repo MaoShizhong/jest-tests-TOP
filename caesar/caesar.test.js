04e2e7c9601a0cd1df2648b054e74b029af71553
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-06-03 17:38:26 +0100

    Write caesar tests

---
+++ caesar/caesar.test.js
@@ -0,0 +1,17 @@
+import { caesar } from './caesar.js';
+
+describe('Encode case-sensitive string via Caesar cipher using given key (ignores non-alphabet characters)', () => {
+    const strings = [
+        ['A', 'B', 1],
+        ['the same', 'the same', 26],
+        ['BaRFoo', 'FeVJss', 4],
+        ['World, say hello!', 'Iadxp, emk tqxxa!', 12],
+        ['barfoo', 'onesbb', 65],
+        ['Tsjkndf34 23!jas!? Hello---?', 'Wvmnqgi34 23!mdv!? Khoor---?', 23455],
+    ];
+
+    test.each(strings)(
+        'Encrypts %j as %j using a key of %d',
+        (plaintext, ciphertext, key) => expect(caesar(plaintext, key)).toBe(ciphertext)
+    );
+});
