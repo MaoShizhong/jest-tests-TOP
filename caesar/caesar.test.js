ee385c27276a372746f71de2213798d4a2dea396
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-06-03 18:01:29 +0100

    Add negative key tests

--- caesar/caesar.test.js
+++ caesar/caesar.test.js
@@ -8,6 +8,8 @@ describe('Encode case-sensitive string via Caesar cipher using given key (ignore
         ['World, say hello!', 'Iadxp, emk tqxxa!', 12],
         ['barfoo', 'onesbb', 65],
         ['Tsjkndf34 23!jas!? Hello---?', 'Wvmnqgi34 23!mdv!? Khoor---?', 23455],
+        ['Negative key', 'Mdfzshud jdx', -1],
+        ['wrAPping NegATivE keY!!!', 'lgPEexcv CtvPIxkT ztN!!!', -37]
     ];
 
     test.each(strings)(
